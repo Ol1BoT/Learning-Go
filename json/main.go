ad9b94a9bb8c2e374e34fcffd6ba0b686fe3bcff
Oliver Mignot <omignot@wenona.nsw.edu.au>
2020-07-23 13:29:38 +1000

    JSON and Pointers

--- json/main.go
+++ json/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 type person struct {
-	FirstName string
-	LastName  string
-	age       int8
+	FirstName string `json:"FirstName"`
+	LastName  string `json:"LastName"`
+	Age       int8   `json:"age"` //originally had this named as age lower case, and the value wasn't being marshalled to json
 }
 
 func main() {
@@ -19,10 +19,21 @@ func main() {
 		31,
 	}
 
-	jn, err := json.Marshal(ollie)
+	jn := toJSON(&ollie)
+
+	fmt.Println(ollie)
+
+	fmt.Println(string(jn))
+
+}
+
+func toJSON(p *person) []byte {
+	jn, err := json.Marshal(p)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(string(jn))
+	// fmt.Println(string(jn))
+
+	return jn
 }
